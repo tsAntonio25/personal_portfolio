ee8eb56e1941ccb4e6be1b0d1818f6cb3a3a2c08
Lala-Lu25 <makesmesmile105@gmail.com>
2026-02-11 23:16:35 +0800

    :art: Added Structures for some sections 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { About } from "./about/about";
 import { TechSkills } from "./tech-skills/tech-skills";
 import { SoftSkills } from "./soft-skills/soft-skills";
 import { Projects } from "./projects/projects";
+import { EducExp } from "./educ-exp/educ-exp";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Intro, About, TechSkills, SoftSkills, Projects],
+  imports: [RouterOutlet, Intro, About, TechSkills, SoftSkills, Projects, EducExp],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
